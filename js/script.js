fc76aa9b26fb46df10e1284b0965ff6280636b25
yurkote <yurokte@gmail.com>
2022-08-07 19:15:36 +0200

    full done w/ details html

--- js/script.js
+++ js/script.js
@@ -128,9 +128,21 @@ $(document).ready(function () {
   }
 });
 
+// $(".header-menu__icon").click(function (event) {
+//   $(this).toggleClass("active");
+//   $(".header-menu").toggleClass("active");
+//   if ($(this).hasClass("active")) {
+//     $("body").data("scroll", $(window).scrollTop());
+//   }
+//   $("body").toggleClass("lock");
+//   if (!$(this).hasClass("active")) {
+//     $("body,html").scrollTop(parseInt($("body").data("scroll")));
+//   }
+// });
+
 $(".header-menu__icon").click(function (event) {
   $(this).toggleClass("active");
-  $(".header-menu").toggleClass("active");
+  $(".menu__list").toggleClass("active");
   if ($(this).hasClass("active")) {
     $("body").data("scroll", $(window).scrollTop());
   }
